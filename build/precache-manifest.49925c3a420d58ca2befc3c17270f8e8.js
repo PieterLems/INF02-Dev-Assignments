81554fb4a34d564d92d60c0da855d9de14baec23
pieterlems <pieterlems.personal@protonmail.com>
2019-05-16 18:51:13 +0200

    small fixes

---
+++ build/precache-manifest.49925c3a420d58ca2befc3c17270f8e8.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "21a0f539d0d0897724de39c2221ff144",
+    "url": "/Natural-User-Interface-WebApplication/index.html"
+  },
+  {
+    "revision": "a00e918be97e4a995177",
+    "url": "/Natural-User-Interface-WebApplication/static/css/2.9e63dcda.chunk.css"
+  },
+  {
+    "revision": "bac59a918218d21e2595",
+    "url": "/Natural-User-Interface-WebApplication/static/css/main.36280e71.chunk.css"
+  },
+  {
+    "revision": "a00e918be97e4a995177",
+    "url": "/Natural-User-Interface-WebApplication/static/js/2.d9833e42.chunk.js"
+  },
+  {
+    "revision": "bac59a918218d21e2595",
+    "url": "/Natural-User-Interface-WebApplication/static/js/main.f60cb141.chunk.js"
+  },
+  {
+    "revision": "6965ae7a929a0f9fdb07",
+    "url": "/Natural-User-Interface-WebApplication/static/js/runtime~main.522b3f74.js"
+  },
+  {
+    "revision": "03ef1918e505c3e3471f9369ef7a638f",
+    "url": "/Natural-User-Interface-WebApplication/static/media/nucleo.03ef1918.eot"
+  },
+  {
+    "revision": "5987dd12fea78ce5f97ae601b08ec03c",
+    "url": "/Natural-User-Interface-WebApplication/static/media/nucleo.5987dd12.woff2"
+  },
+  {
+    "revision": "b17a118e13e53558658b681a0ebdad82",
+    "url": "/Natural-User-Interface-WebApplication/static/media/nucleo.b17a118e.ttf"
+  },
+  {
+    "revision": "f0b489a5dbbff08833d21024f9fcbd4e",
+    "url": "/Natural-User-Interface-WebApplication/static/media/nucleo.f0b489a5.woff"
+  },
+  {
+    "revision": "6fb21bb852150fc83ef4330a6055b460",
+    "url": "/Natural-User-Interface-WebApplication/static/media/react-logo.6fb21bb8.png"
+  }
+]);
